0de64f0dd44339608a4920ee1bc9b18898f1d467
polyanka <polya-nka@ya.ru>
2020-08-23 00:16:59 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h3>Hostels</h3>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>

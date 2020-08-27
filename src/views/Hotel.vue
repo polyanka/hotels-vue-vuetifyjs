e6c4d551a56fec47b23af665ec63f634763b0666
polyanka <polya-nka@ya.ru>
2020-08-27 19:28:38 +0300

    add render addition info

--- src/views/Hotel.vue
+++ src/views/Hotel.vue
@@ -11,6 +11,17 @@
 
     <div v-else>
       <v-row>
+        <v-col>
+          <h2>{{ hostel.name }}</h2>
+        </v-col>
+        <v-col>
+          <span>
+            <v-icon>mdi-map-marker</v-icon>
+            <span>{{ hostel.country }}</span>
+          </span>
+        </v-col>
+      </v-row>
+      <v-row>
         <v-col cols="8" class="mx-auto">
           <v-carousel>
             <v-carousel-item
@@ -25,7 +36,34 @@
       </v-row>
       <v-row justify="center">
         <v-col cols="12">
-          <h3>{{ hostel.name }}</h3>
+          <blockquote class="blockquote">{{ hostel.description }}</blockquote>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="12">
+          <h3 class="pb-4">Most popular facilities</h3>
+          <span v-if="hostel.wifi" class="mx-4">
+            <v-icon color="indigo" class="pr-2">mdi-wifi</v-icon>
+            <span>Free WiFi</span>
+          </span>
+          <span v-if="hostel.parking" class="mx-4">
+            <v-icon color="indigo" class="pr-2">mdi-parking</v-icon>
+            <span>Parking</span>
+          </span>
+          <span v-if="hostel.restaurant" class="mx-4">
+            <v-icon color="indigo" class="pr-2"
+              >mdi-silverware-fork-knife</v-icon
+            >
+            <span>Restaurant</span>
+          </span>
+          <span v-if="hostel.bar" class="mx-4">
+            <v-icon color="indigo" class="pr-2">mdi-glass-cocktail</v-icon>
+            <span>Bar</span>
+          </span>
+          <span v-if="hostel.spa" class="mx-4">
+            <v-icon color="indigo" class="pr-2">mdi-spa</v-icon>
+            <span>Spa</span>
+          </span>
         </v-col>
       </v-row>
     </div>
